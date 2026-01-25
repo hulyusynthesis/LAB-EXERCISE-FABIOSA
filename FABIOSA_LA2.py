f8707cf5fe3f17ddef55ca9b73d3e8dd0ee7cc02
hulyusynthesis <fabiosa.juliusjanven07@gmail.com>
2026-01-25 21:05:27 +0800

    Add files via upload
    Instruction: Create variables to store your personal information
    and display them in the following format:
    
    Fullname (String)
    Age (Integer)
    Height in meters (Float)
    Currently working? (Boolean)

---
+++ FABIOSA_LA2.py
@@ -0,0 +1,9 @@
+name = "Fabiosa Julius Jan Ven"
+age = 19
+height = 1.60
+working = False
+
+print("Fullname:", name, type(name))
+print("Age:", age, type(age))
+print("Height in meters:", height, type(height))
+print("Currently working?", working, type(working))
